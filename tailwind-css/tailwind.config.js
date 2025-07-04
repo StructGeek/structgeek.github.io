6b39107a459a9884a7693ffa6a6af7959e66bc33
StructGeek <78370216+StructGeek@users.noreply.github.com>
2025-07-04 23:17:28 +0530

    add tailwind-css files
    uploaded css and config files required to build tailwind.css

---
+++ tailwind-css/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "../index.html",
+    "../pages/**/*.html",
+    "../assets/html/**/*.html",
+    "../assets/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
